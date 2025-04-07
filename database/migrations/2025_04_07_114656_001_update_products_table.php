7f2ae16e2dc1bf492ee0894f5d584d831181923c
jankrognes <jan@hihostels.no>
2025-04-07 16:29:21 +0200

    Merge remote-tracking branch 'origin/master'

--- database/migrations/2025_04_07_114656_001_update_products_table.php
+++ database/migrations/2025_04_07_114656_001_update_products_table.php
@@ -13,9 +13,15 @@ return new class extends Migration {
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
+            // If the column does not exist, add it.
+            if (! Schema::hasColumn('products', 'store_id')) {
+                $table->unsignedBigInteger('store_id')
+                    ->after('id')
+                    ->comment('Foreign key to the stores table');
+            }
 
-            $table
-                ->foreign('store_id')
+            // Add foreign key constraint ensuring proper spelling of the column name.
+            $table->foreign('store_id', 'products_store_id_foreign')
                 ->references('id')
                 ->on('stores')
                 ->onDelete('cascade')
